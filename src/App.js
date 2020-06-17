2b0e95cf96164974ddf25dff04fa43f98c9dddca
oeisenberg <oliver.eisenberg88@gmail.com>
2020-06-17 18:39:53 +0100

    OE - Strava response sent to backend from URL

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import React from 'react';
+import axios from 'axios';
 import Navbar from './components/layout/Navbar'
 import Register from './components/pages/Register';
+
+function writeToKeysFile(code) {
+    var data = JSON.stringify({"code":code});
+    var config = {
+        method: 'post',
+        url: 'http://localhost:3000/Runlab/oauth',
+        headers: { 
+          'Content-Type': 'application/json',
+        },
+        data : data
+    };
+    axios(config)
+    .then(function (response) {
+        // if (response.data.status !== 200){
+            console.log(response);
+        // }
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+}
 
 class App extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {isLoggedIn: false};
+      const url = window.location.search;
+        try{
+            var r = url.split("&");
+            var code = r[1].split("=")[1];
+            var scopes = r[2].split("=")[1];
+            scopes = scopes.split(",");
+            console.log(scopes) // need to check all scopes requested are accepted
+            writeToKeysFile(code);
+            this.state = {isLoggedIn: true};
+        } catch (err){
+            this.state = {isLoggedIn: false};
+        }
     }
-  
+
     render() {
         const isLoggedIn = this.state.isLoggedIn;
         if (isLoggedIn) {
@@ -16,6 +49,6 @@ class App extends React.Component {
             return <Register />;
         }
     }
-  }
+}
 
 export default App;
