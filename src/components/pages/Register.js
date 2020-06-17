a068783e2ed5724d9f804f407769cc419a00c820
oeisenberg <oliver.eisenberg88@gmail.com>
2020-06-17 13:00:39 +0100

    OE - registration redirects for strava acess codes

--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -1,14 +1,15 @@
-import React, {Component} from 'react'
-import styles from "./styles.css";
-import {Typography} from "@material-ui/core"
+// import axios from 'axios';
+import React from 'react'
 
-class Register extends Component {
+class Register extends React.Component {
+
+    componentDidMount(){
+        window.location.replace("https://www.strava.com/oauth/authorize?client_id=48256&response_type=code&redirect_uri=http://localhost:3000/exchange_token&approval_prompt=force&scope=read_all,activity:read_all");
+    }
+    
     render(){
         return(
-            <div className={styles.contents}>
-                <Typography variant="h3" component="h1">
-                Register Page
-                </Typography>
+            <div className="loginscreen">
             </div>
         )
     }
