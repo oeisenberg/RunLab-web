c65623e5445903a50b3866ed138771f95d77f0f5
oeisenberg <oliver.eisenberg88@gmail.com>
2020-06-22 11:30:34 +0100

    OE - simple run component init

---
+++ src/components/pages/Runs/run.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import styles from "../styles.css";
+import {Typography} from "@material-ui/core";
+
+class Run extends Component {
+    render(){
+        return(
+            <div>
+                <Typography variant="body1" gutterBottom>
+                    Run
+                </Typography>
+            </div>
+        )
+    }
+}
+
+export default Run;
