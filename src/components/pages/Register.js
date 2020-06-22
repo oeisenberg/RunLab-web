a29bd0d39efe300ae591b55326e441351e65ef38
oeisenberg <oliver.eisenberg88@gmail.com>
2020-06-22 11:26:18 +0100

    OE - removed auto redirect for manual

--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -4,12 +4,14 @@ import React from 'react'
 class Register extends React.Component {
 
     componentDidMount(){
-        window.location.replace("https://www.strava.com/oauth/authorize?client_id=48256&response_type=code&redirect_uri=http://localhost:3000/exchange_token&approval_prompt=force&scope=read_all,activity:read_all");
+        // window.location.replace("https://www.strava.com/oauth/authorize?client_id=48256&response_type=code&redirect_uri=http://localhost:3000/exchange_token&approval_prompt=force&scope=read_all,activity:read_all");
     }
     
     render(){
         return(
-            <div className="loginscreen">
+            <div>
+                Please register:<br/>
+                <a href="https://www.strava.com/oauth/authorize?client_id=48256&response_type=code&redirect_uri=http://localhost:3000/exchange_token&approval_prompt=force&scope=read_all,activity:read_all">Click here to redirect</a>
             </div>
         )
     }
