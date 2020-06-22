ec5e5e1c092e150125446039673206aede83d301
oeisenberg <oliver.eisenberg88@gmail.com>
2020-06-22 11:40:55 +0100

    OE - uses promise to check return of ajax

--- src/App.js
+++ src/App.js
@@ -3,7 +3,26 @@ import axios from 'axios';
 import Navbar from './components/layout/Navbar'
 import Register from './components/pages/Register';
 
-function writeToKeysFile(code) {
+async function makeQuery(config){
+    const promise = new Promise(function(resolve, reject){
+        axios(config)
+        .then(function (response) {
+            if (response.data.status !== 200){
+                console.log(response);
+                reject("Error");
+            } else {
+                resolve("Success");
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+            reject("Error");
+        });
+    });
+    return promise;
+}
+
+async function writeToKeysFile(code) {
     var data = JSON.stringify({"code":code});
     var config = {
         method: 'post',
@@ -13,38 +32,58 @@ function writeToKeysFile(code) {
         },
         data : data
     };
-    axios(config)
-    .then(function (response) {
-        // if (response.data.status !== 200){
-            console.log(response);
-        // }
-    })
-    .catch(function (error) {
-        console.log(error);
+
+    return makeQuery(config).then(function() {
+        return true;
+    }, function(err) {
+        return false;
+    });
+}
+
+async function checkLogInStatus() {
+    var config = {
+        method: 'get',
+        url: 'http://localhost:3000/Runlab/oauth',
+        headers: { 
+          'Content-Type': 'application/json',
+        },
+    };
+
+    return makeQuery(config).then(function(){
+        console.log("Registered");
+        return true;
+    }, function(err) {
+        return false;
     });
 }
 
 class App extends React.Component {
     constructor(props) {
-      super(props);
-      const url = window.location.search;
+        super(props);
+        
+        this.state = {
+            isLoggedIn: false,
+        };
+    }
+    
+    async componentDidMount() {
+        var status = false;
         try{
+            const url = window.location.search;
             var r = url.split("&");
             var code = r[1].split("=")[1];
-            var scopes = r[2].split("=")[1];
-            scopes = scopes.split(",");
-            console.log(scopes) // need to check all scopes requested are accepted
-            writeToKeysFile(code);
-            this.state = {isLoggedIn: true};
+            var scopes = r[2].split("=")[1]; // check correct scopes accepted
+
+            status = await writeToKeysFile(code);
         } catch (err){
-            this.state = {isLoggedIn: false};
+            status = await checkLogInStatus();
         }
+        this.setState({isLoggedIn:status});
     }
 
     render() {
-        const isLoggedIn = this.state.isLoggedIn;
-        if (isLoggedIn) {
-            return <Navbar />
+        if(this.state.isLoggedIn){
+            return <Navbar />;
         } else {
             return <Register />;
         }
