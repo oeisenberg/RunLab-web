191b33f05764a54d1670c6549c9f140d387d782b
oeisenberg <oliver.eisenberg88@gmail.com>
2020-06-25 18:27:07 +0100

    OE - updated page template using Grids

--- src/components/pages/Runs/index.js
+++ src/components/pages/Runs/index.js
@@ -1,24 +1,63 @@
 import React, {Component} from 'react';
 import styles from "../styles.css";
+import { makeStyles } from '@material-ui/core/styles';
 import {Typography} from "@material-ui/core";
 import Run from "./run.js";
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
 
-class Runs extends Component {
-    render(){
-        return(
-            <div className={styles.contents}>
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      elevation: 1,
+    },
+  }));
+
+export default function Runs() {
+    const classes = useStyles();
+
+    return(
+        <div className={styles.contents}>
+        <div className={classes.root}>
+        <Grid container spacing={3}>
+            <Grid item xs={12}>
+                <Typography variant="overline">
+                    heat map
+                </Typography>
+            </Grid>
+            <Grid item xs={12}>
                 <Typography variant="h3" component="h1">
                     Runs Page
                 </Typography>
-                <div className="run">
-                    <Run />
-                </div>
-                <div className="run">
-                    <Run />
-                </div>
-            </div>
-        )
-    }
+            </Grid>
+            <Grid item xs={12}>
+                <Paper className={classes.paper}><Run /></Paper>
+            </Grid>
+            <Grid item xs={12}>
+                <Paper className={classes.paper}><Run /></Paper>
+            </Grid>
+            <Grid item xs={12}>
+                <Paper className={classes.paper}><Run /></Paper>
+            </Grid>
+            <Grid item xs={12}>
+                <Paper className={classes.paper}><Run /></Paper>
+            </Grid>
+            <Grid item xs={12}>
+                <Paper className={classes.paper}><Run /></Paper>
+            </Grid>
+            <Grid item xs={12}>
+                <Paper className={classes.paper}><Run /></Paper>
+            </Grid>
+        </Grid>
+        </div>
+        </div>
+    )
+
 }
 
-export default Runs;
+// export default Runs;
