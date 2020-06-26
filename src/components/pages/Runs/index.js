f3d7896d5c59a83b89f9ca126f4a507f92f3797b
oeisenberg <oliver.eisenberg88@gmail.com>
2020-06-26 15:58:14 +0100

    OE - using content div, cleaning imports

--- src/components/pages/Runs/index.js
+++ src/components/pages/Runs/index.js
@@ -1,11 +1,10 @@
-import React, {Component} from 'react';
+import React from 'react';
 import styles from "../styles.css";
 import { makeStyles } from '@material-ui/core/styles';
 import {Typography} from "@material-ui/core";
 import Run from "./run.js";
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -13,7 +12,7 @@ const useStyles = makeStyles((theme) => ({
     },
     paper: {
       padding: theme.spacing(2),
-      textAlign: 'center',
+      textAlign: 'left',
       elevation: 1,
     },
   }));
@@ -22,37 +21,37 @@ export default function Runs() {
     const classes = useStyles();
 
     return(
-        <div className={styles.contents}>
+        <div class="contents">
         <div className={classes.root}>
-        <Grid container spacing={3}>
-            <Grid item xs={12}>
-                <Typography variant="overline">
-                    heat map
-                </Typography>
-            </Grid>
-            <Grid item xs={12}>
-                <Typography variant="h3" component="h1">
-                    Runs Page
-                </Typography>
-            </Grid>
-            <Grid item xs={12}>
-                <Paper className={classes.paper}><Run /></Paper>
-            </Grid>
-            <Grid item xs={12}>
-                <Paper className={classes.paper}><Run /></Paper>
-            </Grid>
-            <Grid item xs={12}>
-                <Paper className={classes.paper}><Run /></Paper>
-            </Grid>
-            <Grid item xs={12}>
-                <Paper className={classes.paper}><Run /></Paper>
-            </Grid>
-            <Grid item xs={12}>
-                <Paper className={classes.paper}><Run /></Paper>
-            </Grid>
-            <Grid item xs={12}>
-                <Paper className={classes.paper}><Run /></Paper>
-            </Grid>
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <Typography variant="overline">
+                        heat map
+                    </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <Typography variant="h3" component="h1">
+                        Runs Page
+                    </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <Paper className={classes.paper}><Run /></Paper>
+                </Grid>
+                <Grid item xs={12}>
+                    <Paper className={classes.paper}><Run /></Paper>
+                </Grid>
+                <Grid item xs={12}>
+                    <Paper className={classes.paper}><Run /></Paper>
+                </Grid>
+                <Grid item xs={12}>
+                    <Paper className={classes.paper}><Run /></Paper>
+                </Grid>
+                <Grid item xs={12}>
+                    <Paper className={classes.paper}><Run /></Paper>
+                </Grid>
+                <Grid item xs={12}>
+                    <Paper className={classes.paper}><Run /></Paper>
+                </Grid>
         </Grid>
         </div>
         </div>
