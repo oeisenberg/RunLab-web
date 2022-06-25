3124d2fb5c6c668fc13359fd3491744c237ce912
Oliver <oliver.eisenberg88@gmail.com>
2022-06-25 16:36:46 +0100

    Removing strict mode

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 import Controller from "./Controller";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const themeLight = createTheme({
+  palette: {
+      mode: 'light',
+      background: {
+          default: "#e4f0e2"
+      }
+  }
+});
+
+const themeDark = createTheme({
+  palette: {
+      mode: 'dark',
+      background: {
+          default: "#222222"
+      },
+      text: {
+          primary: "#ffffff"
+      }
+  }
+});
+
 root.render(
-  <React.StrictMode>
-    <Controller />
-  </React.StrictMode>
+  // was making componets mount x2
+  //<React.StrictMode>
+    <ThemeProvider theme={themeDark}> 
+      <CssBaseline />
+      <Controller />
+    </ThemeProvider>
+  //</React.StrictMode>
 );
