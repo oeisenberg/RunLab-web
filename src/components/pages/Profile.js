65b5f640748ee06e635d20854149ffb206fc4bd9
Oliver <oliver.eisenberg88@gmail.com>
2022-07-03 21:03:35 +0100

    Basic code to populate the page

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Card from "../layout/BasicCard";
+import Typography from '@mui/material/Typography';
 
 export default function Profile() {
 
+  const [statistics, setStatistics] = useState(null);
+
+  useEffect(() => {
+    if (statistics === null) {
+      // queryRunLab("");
+    }
+  });
+
+  const queryRunLab = async (query) => {
+    try {
+      await fetch("http://localhost:8080/Runlab/" + query)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data.body);
+          setStatistics(data.body);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div>
-
+      <br/>
+      <Typography variant="h3" component="h1">
+        Profile
+      </Typography>
+      <br/>
+      <Card width={300} title="" body="" />
        
   
     </div>
