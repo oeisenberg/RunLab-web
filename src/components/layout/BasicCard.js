20558f5318dde07ba8a8a80ec1392c445a245f5d
Oliver <oliver.eisenberg88@gmail.com>
2022-07-03 21:03:09 +0100

    Dyn card component

---
+++ src/components/layout/BasicCard.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+
+export default function BasicCard(props) {
+  return (
+    <Card sx={{ width:props.width}}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          {props.title}
+        </Typography>
+        <Typography variant="h5" component="div">
+          {props.body}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
