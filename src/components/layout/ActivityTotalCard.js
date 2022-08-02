d5e227b5df423dd0993a425bd792228600fd6af1
Oliver <oliver.eisenberg88@gmail.com>
2022-08-02 11:38:30 +0100

    activity totals shown on cards

--- src/components/layout/ActivityTotalCard.js
+++ src/components/layout/ActivityTotalCard.js
@@ -4,6 +4,12 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 
+// https://stackoverflow.com/questions/7342957/how-do-you-round-to-1-decimal-place-in-javascript
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
+
 export default function ActivityTotalCard(props) {
   return (
     <Card sx={{ width: props.width, height: props.height }}>
@@ -22,13 +28,13 @@ export default function ActivityTotalCard(props) {
           {"Total: " + props.count}
         </Typography>
         <Typography variant="h8" component="div">
-          {"Distance: " + props.distance}
+          {"Distance: " + round(props.distance / 1000, 2) + " km"}
         </Typography>
         <Typography variant="h8" component="div">
-          {"moving_time: " + props.moving_time}
+          {"moving_time: " + round(props.moving_time / 60, 1) + " mins"}
         </Typography>
         <Typography variant="h8" component="div">
-          {"elapsed_time: " + props.elapsed_time}
+          {"elapsed_time: " + round(props.elapsed_time / 60, 1) + " mins"}
         </Typography>
         <Typography variant="h8" component="div">
           {"achievement_count: " + props.achievement_count}
