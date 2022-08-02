652f4e0c6df78511254e0c05388c179c8dcafd05
Oliver <oliver.eisenberg88@gmail.com>
2022-08-02 18:46:21 +0100

    spelling mistake on path

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
       date.setDate(date.getDate() - 28);
       // use date to get the last 4 wks of activity
 
-      queryRunLab("athelete/activities", saveUserActivityData);
+      queryRunLab("athlete/activities", saveUserActivityData);
     }
   }, [userActivityData]);
 
