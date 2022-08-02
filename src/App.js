fb730a76647efbf8cff6c27a9370f33db7e5211c
Oliver <oliver.eisenberg88@gmail.com>
2022-08-02 16:04:24 +0100

    Corrected for api endpoint changes with auth token + refactroring

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./Styles.css";
 import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import axios from "axios";
 import HomePage from "./components/pages/Home";
 import DashboardPage from "./components/pages/Dashboard";
 import RunsPage from "./components/pages/Runs/index";
@@ -10,44 +11,54 @@ import Navbar from "./components/layout/Navbar";
 
 function App() {
   const [userProfileData, setUserProfileData] = useState(null);
+
+  useEffect(() => {
+    if (userProfileData === null) {
+      queryRunLab("athlete/profile", saveUserProfileData);
+    }
+  }, []);
+
   const [userStatisticsData, setUserStatisticsData] = useState(null);
 
-  const queryRunLab = async (query, fcn) => {
-    try {
-      await fetch("http://localhost:8080/Runlab/" + query)
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-          console.log(data);
-          fcn(data)
-        });
-    } catch (err) {
-      console.log(err);
+  useEffect(() => {
+    if (userProfileData !== null) {
+      queryRunLab(
+        "athlete/statistics?ID=" + userProfileData.id,
+        saveUserStatisticsData
+      );
     }
+  }, [userProfileData]);
+
+  const queryRunLab = async (query, fcn) => {
+    var config = {
+      method: "get",
+      url: "http://localhost:8080/runlab-api/v1/" + query,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + sessionStorage.getItem("token"),
+      },
+    };
+
+    const promise = new Promise((resolve, reject) => {
+      axios(config).then((response) => {
+        fcn(response.data.body);
+      });
+    });
+
+    return promise;
   };
 
   const saveUserProfileData = (data) => {
-    return setUserProfileData(data.body);
+    return setUserProfileData(data);
   };
 
   const saveUserStatisticsData = (data) => {
-    return setUserStatisticsData(data.body);
+    return setUserStatisticsData(data);
   };
-  
-  useEffect(() => {
-    if (userProfileData === null) {
-      queryRunLab("getAtheleteProfile", saveUserProfileData);
-    }
-
-    if (userProfileData !== null && userStatisticsData === null) {
-      queryRunLab("getAtheleteStatistics/" + userProfileData.id, saveUserStatisticsData);
-    }
-  });
 
   return (
     <BrowserRouter>
-      <Navbar Profile={userProfileData}/>
+      <Navbar Profile={userProfileData} />
       <div className="Page-Content">
         <Routes>
           <Route path="/" element={<HomePage />}></Route>
@@ -55,7 +66,10 @@ function App() {
           <Route path="/Dashboard" element={<DashboardPage />}></Route>
           <Route path="/Runs" element={<RunsPage />}></Route>
           <Route path="/About" element={<AboutPage />}></Route>
-          <Route path="/Profile" element={<ProfilePage Statistics={userStatisticsData}/>}></Route>
+          <Route
+            path="/Profile"
+            element={<ProfilePage Statistics={userStatisticsData} />}
+          ></Route>
         </Routes>
       </div>
     </BrowserRouter>
