47e63c3b385e10652f050378b8f07da0a1b97e5e
Oliver <oliver.eisenberg88@gmail.com>
2022-08-02 11:38:47 +0100

    Simplified blank runs page

--- src/components/pages/Runs/index.js
+++ src/components/pages/Runs/index.js
@@ -1,32 +1,12 @@
-import React from 'react';
+import React from "react";
+import Typography from "@mui/material/Typography";
 
 export default function Runs() {
-
-    return(
-        <div>
-            Runs
-                {/* <Grid container spacing={3}>
-                    <Grid item xs={12}>
-                        <Typography variant="overline">
-                            heat map
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Typography variant="h3" component="h1">
-                            Runs Page
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <RunSummary runtitle="Example Run 1" runsummary="Run Summary" runid="0" runmap="map"/>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <RunSummary runtitle="Sprints" runsummary="Run Summary" runid="1" runmap="map"/>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <RunSummary runtitle="Run Title" runsummary="Run Summary" runid="2" runmap="map"/>
-                    </Grid>
-                </Grid> */}
-            
-        </div>
-    )
+  return (
+    <div>
+      <Typography variant="h3" component="h1">
+        Runs
+      </Typography>
+    </div>
+  );
 }
