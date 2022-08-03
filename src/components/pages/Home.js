e0ba451b488b33ef054374d3f0c55689758d483d
Oliver <oliver.eisenberg88@gmail.com>
2022-08-03 21:17:51 +0100

    Remove top plot, pass more activity cards

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,8 +3,6 @@ import "../../Styles.css";
 import CardSummary from "../layout/ActivitySummaryCard";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
-import Plot from "react-plotly.js";
-
 
 function Home(props) {
 
@@ -12,12 +10,6 @@ function Home(props) {
     return <div></div>;
   }
 
-  const x=[], distance=[], speed=[];
-  Object.keys(props.ActivityData).map((key, idx) => (
-    x.push(idx+1),
-    distance.push(props.ActivityData[key].distance),
-    speed.push((props.ActivityData[key].distance/1000)/(props.ActivityData[key].movingTime/3600))
-  ))
 
   return (
     <div>
@@ -26,44 +18,12 @@ function Home(props) {
       </Typography>
       <br />
       <Stack direction="column" spacing={2} sx={{ ml: 5, mr: 5 }}>
-        <Plot
-          data={[
-            {
-              x: x,
-              y: distance,
-              type: "bar",
-              name: "distance",
-              marker: { color: "blue" },
-            },
-             {
-              x: x,
-              y: speed,
-              yaxis: 'y2',
-              type: "line",
-              name: "speed",
-              mode: "lines+markers",
-              marker: { color: "red" },
-            },
-          ]}
-          layout={{
-            height: 450,
-            title: "Last 10 Activities",
-            paper_bgcolor: "rgba(0,0,0,0.1)",
-            plot_bgcolor: "rgba(0,0,0,0.1)",
-            yaxis2: {
-              overlaying: 'y',
-              side: 'right'
-            }
-          }}
-          config={{
-            displayModeBar: false,
-          }}
-          useResizeHandler={true}
-        />
         {Object.keys(props.ActivityData).map((key, idx) => (
           <CardSummary
             key={key}
-            title={idx+1 + ") " + props.ActivityData[key].name}
+            title={props.ActivityData[key].name}
+            startLatLng={props.ActivityData[key].start_latlng}
+            polyline={props.ActivityData[key].map.summary_polyline}
             distance={props.ActivityData[key].distance}
             startDateLocal={props.ActivityData[key].start_date_local}
             movingTime={props.ActivityData[key].moving_time}
